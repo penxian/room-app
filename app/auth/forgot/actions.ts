c0fbd5a9e91b83438c30695e1e68e3173f735127
pen <blueskyred2006@163.com>
2025-05-15 21:54:40 +0800

    使用use server就不能使用edge

--- app/auth/forgot/actions.ts
+++ app/auth/forgot/actions.ts
@@ -1,7 +1,5 @@
 'use server'
 
-export const runtime = 'edge'
-
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,7 +1,5 @@
 'use server'
 
-export const runtime = 'edge'
-
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
