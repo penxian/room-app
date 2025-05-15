6955939d0077a6c10d4dc65b8fab8c19ad01652a
pen <blueskyred2006@163.com>
2025-05-15 21:24:05 +0800

    新增edge

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge'
 import { createClient } from '@/utils/supabase/server'
 import { revalidatePath } from 'next/cache'
 import { type NextRequest, NextResponse } from 'next/server'
