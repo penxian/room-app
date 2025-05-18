9ef964fbd50dc79f8991b3e8692ac189ee782141
pen <blueskyred2006@163.com>
2025-05-18 22:04:21 +0800

    新增edge

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
-
+export const runtime = 'edge'
 export default async function Home() {
   const supabase = createClient()
   const {
